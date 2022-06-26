6d3520c28f17b54941439231b164c6b62374ab38
alexis tapia <alexist8888@gmail.com>
2022-06-25 22:11:35 -0700

    update the main page
    allowed the ability to create and add more boards

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from './components/Home Page Comp/Header';
 import Body from './components/Home Page Comp/Body';
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/"  element = {
+        <Route path="/"  element = { 
             <div className="App">
               <Header />
               <Body />
--- my-app/src/components/Home Page Comp/Body.js
+++ my-app/src/components/Home Page Comp/Body.js
@@ -1,15 +1,12 @@
+/* eslint-disable react/jsx-pascal-case */
+import Create_Buttons from "./Create_Buttons";
+
 const Body = () => {
   return (
     <div id="content-body">
       <div id="Your-Boards">Your Boards</div>
       <div id="Boards-container">
-        <div id="create-board-container">
-          <div id="create-board-overlay">
-            <input className="create-board" type="text" placeholder="Name" />
-            <button id="create-board-button">Create</button>
-          </div>
-          <div id="create-board">+ Create Board</div>
-        </div>
+        <Create_Buttons />
       </div>
     </div>
   );
